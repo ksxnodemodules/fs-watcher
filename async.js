563d576e0467222b8c16e76514e7c21367603290
khai96_ <hvksmr1996@gmail.com>
2016-03-16 12:09:11 +0700

    update

--- async.js
+++ async.js
@@ -23,17 +23,39 @@
 		var createPromiseCallback = (...args) =>
 			(...subargs) => readFile(storage, createReadFileCallback(...args, ...subargs));
 
-		var createReadFileCallback = (files, callback, resolve) =>
-			(...previous) => createFileCheckPromise(files, ...previous).then(resolve); // <-- Continue from here...
+		var createReadFileCallback = (files, ...promise) =>
+			(...previous) => createFileCheckPromise(files, ...previous).then(createCallbackCaller(...promise));
 
 		var createFileCheckPromise = (files, ...previous) =>
 			Promise.all(files.map(createMapCallback(createPrevious(...previous))));
+
+		var createCallbackCaller = (...args) =>
+			(changes) => repeatCallback(changes.filter(Boolean), ...args);
 
 		var createMapCallback = (previous) =>
 			(fname) => createSubPromise(fname, previous);
 
 		var createPrevious = (error, data) =>
 			error ? create(null) : parseJSON(String(data));
+
+		var repeatCallback = (changes, callback, resolve, reject) => {
+			// var intervalCallback = () =>
+			// 	changes.length ? callback(changes, subresolve, subreject) : subresolve();
+			var repeat = () => {
+				if (changes.length) {
+					// continue from here...
+				}
+			};
+			var loop = setInterval(repeat);
+			var subresolve = (...args) => {
+				clearInterval(loop);
+				resolve(...args);
+			};
+			var subreject = (...args) => {
+				clearInterval(loop);
+				reject(...args);
+			};
+		};
 
 		var createSubPromise = (fname, previous) =>
 			new Promise(createSubPromiseCallback(resolvePath(fname), previous));
