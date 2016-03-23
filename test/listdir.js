2ec613c619ba28fee516c4bcb19d7e74c6f0c014
khai96_ <hvksmr1996@gmail.com>
2016-03-23 22:45:51 +0700

    update test/listdir

--- test/listdir.js
+++ test/listdir.js
@@ -2,12 +2,15 @@
 ((module) => {
 	'use strict';
 
-	var joinPath = require('path').join;
+	var path = require('path');
 	var readdirSync = require('fs').readdirSync;
+
+	var resolvePath = path.resolve;
+	var joinPath = path.join;
 
 	var listdir = (dirname) => {
 		try {
-			return readdirSync(dirname).map((item) => joinPath(dirname, item));
+			return readdirSync(dirname).map((item) => resolvePath(joinPath(dirname, item)));
 		} catch (error) {
 			return [];
 		}
