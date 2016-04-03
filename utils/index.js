3c28245b1b3e98bb5421ec4a9bc9901d5b73a8c4
khai96_ <hvksmr1996@gmail.com>
2016-04-03 12:23:13 +0700

    add fsw-array

--- utils/index.js
+++ utils/index.js
@@ -3,5 +3,6 @@ module.exports = {
     'ChangeDetail': require('./change-detail.js'),
     'createChangeDetail': require('./create-change-detail.js'),
     'resolvePathArray': require('./resolve-path-array.js'),
+    'FSWArray': require('./fsw-array.js'),
     '__proto__': null
 }
