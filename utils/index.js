b2638b3583b545c25ce0b6b852cfe7e6efd649fd
khai96_ <hvksmr1996@gmail.com>
2016-04-03 13:01:50 +0700

    add json-separator

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,7 @@ module.exports = {
     'ChangeDetail': require('./change-detail.js'),
     'createChangeDetail': require('./create-change-detail.js'),
     'resolvePathArray': require('./resolve-path-array.js'),
+    'jsonSeperator': require('./json-separator.js'),
     'FSWArray': require('./fsw-array.js'),
     '__proto__': null
 }
