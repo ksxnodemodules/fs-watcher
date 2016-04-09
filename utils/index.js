6448cade1958b98682445f598291071b7ee93741
khai96_ <hvksmr1996@gmail.com>
2016-04-09 17:46:29 +0700

    add promise

--- utils/index.js
+++ utils/index.js
@@ -4,6 +4,7 @@ module.exports = {
     'createChangeDetail': require('./create-change-detail.js'),
     'resolvePathArray': require('./resolve-path-array.js'),
     'jsonSeperator': require('./json-separator.js'),
+    'Promise': require('./promise'),
     'FSWArray': require('./fsw-array.js'),
     '__proto__': null
 }
