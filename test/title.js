1acd7fab87845d6323eb4b037e7fcae57799ed5b
khai96_ <hvksmr1996@gmail.com>
2016-04-16 22:05:06 +0700

    update test/title.js

--- test/title.js
+++ test/title.js
@@ -1,3 +1,12 @@
 
-module.exports = (tname) =>
-    console.log(`${'_'.repeat(35)}\nTESTING ${tname}`);
+
+((module) => {
+    'use strict';
+
+    const _x35 = '_'.repeat(35);
+
+    module.exports = (tname) =>
+        console.log(`\x1B[36m${_x35}\nTESTING${tname ? '\x20' + tname : ''}:\x1B[0m`);
+
+
+})(module);
