97b7648648054bb1f5888ad97f171caa94784d9a
khai96_ <hvksmr1996@gmail.com>
2016-04-16 18:55:41 +0700

    change testing order

--- test/main.js
+++ test/main.js
@@ -8,7 +8,7 @@
 		`TESTING:\n\t\x1B[33mType:\x1B[0m NodeJS package\n\t\x1B[33mName:\x1B[0m ${info.name}\n\t\x1B[33mGit URL:\x1B[0m ${info.repository.url}`
 	);
 
-	['async', 'sync']
+	['sync', 'async']
 		.forEach((name) => setTimeout(() => require(`./${name}/main.js`)));
 
 })(module);
